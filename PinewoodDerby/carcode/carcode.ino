8ed565da51ac2b2b7ee919c8473707cbb796cb1a
crowcoder <crowcoder@gmail.com>
2021-01-03 13:05:21 -0500

    made finishline sequence dynamic with multiples of 8 LEDs. Correctecd schematic

--- PinewoodDerby/carcode/carcode.ino
+++ PinewoodDerby/carcode/carcode.ino
@@ -2,8 +2,8 @@
 #include "carstate.h"
 
 const int run_duration = 3000;
-const int maxBrightness = 60;
-const int runningBrightness = 50;
+const int maxBrightness = 60; // up to 255. More bright, more amps
+const int runningBrightness = 50; // up to 255. More bright, more amps
 const int LED_PIN = 7;
 const int NUM_LEDS = 8;
 CRGB leds[NUM_LEDS];
@@ -11,6 +11,7 @@ CRGB leds[NUM_LEDS];
 const int E1 = 5; // Enable Pin for motor
 const int I1 = 3; // Control pin 1 for motor
 const int I2 = 4; // Control pin 2 for motor
+const int running_speed = 200; //speed to run motors
 
 const int E2 = 8;
 const int I3 = 9;
@@ -123,11 +124,9 @@ void StopMotor()
 
 void StartMotor()
 {
-  Serial.println("Get your motor running..!");
-
   //Set motors speed
-  analogWrite(E1, 255);
-  analogWrite(E2, 255);
+  analogWrite(E1, running_speed);
+  analogWrite(E2, running_speed);
 
   //set rotation direction
   digitalWrite(I1, HIGH);
@@ -142,105 +141,55 @@ void finishLineLEDSequence()
   FastLED.setBrightness(maxBrightness);
   unsigned long now = millis();
   if (now - lastFinishedTimestamp > 100) {
-    switch (finishLineLEDIteration) {
-      case 0:
-        leds[0] = CRGB(0, 255, 0);
-        leds[1] = CRGB(0, 0, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 0, 0);
-        leds[7] = CRGB(0, 255, 0);
-        finishLineLEDIteration = 1;
-        break;
-      case 1:
-        leds[0] = CRGB(0, 255, 0);
-        leds[1] = CRGB(0, 255, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 255, 0);
-        leds[7] = CRGB(0, 255, 0);
-        finishLineLEDIteration = 2;
-        break;
-      case 2:
-        leds[0] = CRGB(0, 255, 0);
-        leds[1] = CRGB(0, 255, 0);
-        leds[2] = CRGB(0, 255, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 255, 0);
-        leds[6] = CRGB(0, 255, 0);
-        leds[7] = CRGB(0, 255, 0);
-        finishLineLEDIteration = 3;
-        break;
-      case 3:
-        leds[0] = CRGB(0, 255, 0);
-        leds[1] = CRGB(0, 255, 0);
-        leds[2] = CRGB(0, 255, 0);
-        leds[3] = CRGB(0, 255, 0);
-        leds[4] = CRGB(0, 255, 0);
-        leds[5] = CRGB(0, 255, 0);
-        leds[6] = CRGB(0, 255, 0);
-        leds[7] = CRGB(0, 255, 0);
-        finishLineLEDIteration = 4;
-        break;
-      case 4:
-        leds[0] = CRGB(0, 0, 0);
-        leds[1] = CRGB(0, 0, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 255, 0);
-        leds[4] = CRGB(0, 255, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 0, 0);
-        leds[7] = CRGB(0, 0, 0);
-        finishLineLEDIteration = 5;
-        break;
-      case 5:
-        leds[0] = CRGB(0, 0, 0);
-        leds[1] = CRGB(0, 0, 0);
-        leds[2] = CRGB(0, 255, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 255, 0);
-        leds[6] = CRGB(0, 0, 0);
-        leds[7] = CRGB(0, 0, 0);
-        finishLineLEDIteration = 6;
-        break;
-      case 6:
-        leds[0] = CRGB(0, 0, 0);
-        leds[1] = CRGB(0, 255, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 255, 0);
-        leds[7] = CRGB(0, 0, 0);
-        finishLineLEDIteration = 7;
-        break;
-      case 7:
-        leds[0] = CRGB(0, 255, 0);
-        leds[1] = CRGB(0, 0, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 0, 0);
-        leds[7] = CRGB(0, 255, 0);
-        finishLineLEDIteration = 8;
-      case 8:
-        leds[0] = CRGB(0, 0, 0);
-        leds[1] = CRGB(0, 0, 0);
-        leds[2] = CRGB(0, 0, 0);
-        leds[3] = CRGB(0, 0, 0);
-        leds[4] = CRGB(0, 0, 0);
-        leds[5] = CRGB(0, 0, 0);
-        leds[6] = CRGB(0, 0, 0);
-        leds[7] = CRGB(0, 0, 0);
-        finishLineLEDIteration = 0;
-        break;
+
+    for (int i = 0; i < 8 / NUM_LEDS; i++) {
+      int m = i * 8; //multiplier to shorten code
+
+      switch (finishLineLEDIteration) {
+        case 0:
+          leds[0 + m] = leds[7 + m] = CRGB::Green;
+          leds[1 + m] = leds[2 + m] = leds[3 + m] = leds[4 + m] = leds[5 + m] = leds[6 + m] = CRGB(0, 0, 0);
+          finishLineLEDIteration = 1;
+          break;
+        case 1:
+          leds[0 + m] = leds[1 + m] = leds[6 + m] = leds[7 + m] = CRGB::Green;
+          leds[2 + m] = leds[3 + m] = leds[4 + m] = leds[5 + m] = CRGB(0, 0, 0);
+          finishLineLEDIteration = 2;
+          break;
+        case 2:
+          leds[0 + m] = leds[1 + m] = leds[2 + m] = leds[5 + m] = leds[6 + m] = leds[7 + m] = CRGB::Green;
+          leds[3 + m] = leds[4 + m] = CRGB(0, 0, 0);
+          finishLineLEDIteration = 3;
+          break;
+        case 3:
+          leds[0 + m] = leds[1 + m] = leds[2 + m] = leds[3 + m] = leds[4 + m] = leds[5 + m] = leds[6 + m] = leds[7 + m] = CRGB::Green;
+          finishLineLEDIteration = 4;
+          break;
+        case 4:
+          leds[0 + m] = leds[1 + m] = leds[2 + m] = leds[5 + m] = leds[6 + m] = leds[7 + m] = CRGB(0, 0, 0);
+          leds[3 + m] = leds[4 + m] = CRGB::Green;
+          finishLineLEDIteration = 5;
+          break;
+        case 5:
+          leds[0 + m] = leds[1 + m] = leds[3 + m] = leds[4 + m] = leds[6 + m] = leds[7 + m] = CRGB(0, 0, 0);
+          leds[2 + m] = leds[5 + m] = CRGB::Green;
+          finishLineLEDIteration = 6;
+          break;
+        case 6:
+          leds[0 + m] = leds[2 + m] = leds[3 + m] = leds[4 + m] = leds[5 + m] = leds[7 + m] = CRGB(0, 0, 0);
+          leds[1 + m] = leds[6 + m] = CRGB::Green;          
+          finishLineLEDIteration = 7;
+          break;
+        case 7:
+          leds[0 + m] = leds[7 + m] = CRGB::Green;
+          leds[1 + m] = leds[2 + m] = leds[3 + m] = leds[4 + m] = leds[5 + m] = leds[6 + m] = CRGB(0, 0, 0);          
+          finishLineLEDIteration = 8;
+          break;
+        case 8:
+          leds[0] = leds[1] = leds[2] = leds[3] = leds[4] = leds[5] = leds[6] = leds[7] = CRGB(0, 0, 0);
+          finishLineLEDIteration = 0;
+          break;
+      }
     }
     FastLED.show();
     lastFinishedTimestamp = millis();
@@ -254,7 +203,7 @@ void runningLEDSequence()
   if (now - lastRunningTimestamp > 100) {
     FastLED.setBrightness(runningBrightness);
     for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(255, 0, 0);
+      leds[i] = CRGB::Red;
     }
     lastRunningTimestamp = millis();
   } else {
